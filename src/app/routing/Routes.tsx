2f161d5007d2f3acd530a3b7ca3d4bfedffe1f34
Asker <kotsevasker0@gmail.com>
2025-08-31 00:35:30 +0300

    setting routes

--- src/app/routing/Routes.tsx
+++ src/app/routing/Routes.tsx
@@ -1,3 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
+import Main from "../../pages/Main/Main";
+import SignIn from "../../pages/SignIn/SignIn";
+import SignUp from "../../pages/SignUp/SignUp";
 
-export const Routes = createBrowserRouter([])
+
+export const Routes = createBrowserRouter([
+  {
+    path:'/',
+    element:<Main/>,
+  },
+  {
+    path:'/auth/signin',
+    element:<SignIn/>
+  },
+  {
+    path:"/auth/signup",
+    element:<SignUp/>
+  }
+])
